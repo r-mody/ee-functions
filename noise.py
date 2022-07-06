deacb58523483907ca9a5d6b73a3266bef52a44c
raj <mody.raj@outlook.com>
2022-07-05 22:16:24 -0400

    Added noise figure calculation

--- noise.py
+++ noise.py
@@ -1,10 +1,9 @@
-from asyncio import constants
 import math
 
 k = 1.38e-23 # Boltzmann's Constant (J/K)
 
-def thermal_noise(R, fH, fL, T=25):
-    """Calculates the Johnson Thermal Noise in Joules (J)
+def thermal_noise(R, fH, fL, T=298):
+    """Calculates the Johnson Thermal Noise in mV (RMS)
 
     Args:
         R (float): Resistance in Ohms
@@ -13,7 +12,19 @@ def thermal_noise(R, fH, fL, T=25):
         T (float): Temperature in Kelvin (K) default set to 25°C
 
     Returns:
-        float: Thermal noise in Joules
+        float: Thermal noise in V (RMS)
     """
     return math.sqrt(4*k*T*R*(fH-fL))
 
+
+def cal_F(snr_i, snr_o):
+    """Calculate the noise figure
+
+    Args:
+        snr_i (float): Signal to Noise (SNR) ratio of input
+        snr_o (float): Signal to Noise (SNR) ratio of output
+
+    Returns:
+        float: Noise Figure F
+    """
+    return snr_i/snr_o
